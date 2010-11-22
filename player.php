542c92458913436b191b847995a9bc779c553cae
Mo McRoberts <mo.mcroberts@nexgenta.com>
2010-11-22 08:21:55 +0000

    Add 'player' base handler.

---
+++ player.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * media: The media metadata model
+ *
+ * Copyright 2010 Mo McRoberts.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+require_once(dirname(__FILE__) . '/model.php');
+
+class MediaPlayer extends Page
+{
+	protected $defaultSkin = 'media';
+	protected $templateName = 'player.phtml';
+	protected $modelClass = 'Media';
+
+	public $episode;
+
+	protected function getObject()
+	{
+		if(!isset($this->object))
+		{
+			if(null !== ($tag = $this->request->consume()))
+			{
+				$this->object = $this->model->locateObject($tag, false, 'version');
+				if($this->object === null)
+				{
+					return $this->error(Error::OBJECT_NOT_FOUND);
+				}
+			}
+			else
+			{
+				return $this->error(Error::NO_OBJECT);
+			}
+		}
+		if(!isset($this->episode))
+		{
+			$this->episode = $this->object['parent'];
+		}
+		return true;
+	}
+
+	protected function assignTemplate()
+	{
+		parent::assignTemplate();
+
+		$this->vars['episode'] = $this->episode;
+		$this->vars['version'] = $this->object;
+	}
+}
